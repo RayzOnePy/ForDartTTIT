dd6febfe13f39017383f34a23551fe46656fb838
RayzOne <99681039+RayzOnePy@users.noreply.github.com>
2022-11-02 17:50:40 +0700

    Add files via upload

---
+++ prac1.4/bin/dart_application_1.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main() {
+  // Task 1
+
+  // int n = int.parse(stdin.readLineSync()!);
+  // int total = 1;
+  // for (int i = 3; i <= n; i += 3) {
+  //   total *= i;
+  // }
+  // print(total);
+
+  // Task 5
+
+  // print("Введите X");
+  // int x = int.parse(stdin.readLineSync()!);
+  // print("Введите Y");
+  // int y = int.parse(stdin.readLineSync()!);
+
+  // if (x > -1 && x < 3) {
+  //   if (y > -2 && y < 4) {
+  //     print("Принадлежит");
+  //   } else {
+  //     print("Не принадлежит");
+  //   }
+  // } else {
+  //   print("Не принадлежит");
+  // }
+}
