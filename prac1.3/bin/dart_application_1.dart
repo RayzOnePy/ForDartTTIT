3df21423dd44591f568ea23a85307c3c2c6b1be5
RayzOne <99681039+RayzOnePy@users.noreply.github.com>
2022-12-24 17:49:10 +0700

    Update dart_application_1.dart

--- prac1.3/bin/dart_application_1.dart
+++ prac1.3/bin/dart_application_1.dart
@@ -83,4 +83,29 @@ void main() {
   // }
   // print(nums);
   // file.writeAsString("$nums");
+  
+  // Task 3
+  //   var fileNums = File("files/height.txt");
+  //   var content = fileNums.readAsStringSync();
+  //   List<String> nums = content.split(" ");
+  //   print(nums);
+  //   int container = 0;
+  //   for(int i = 0; i < nums.length; i++){
+  //     for(int j = 1; j < nums.length; j++){
+  //       int tempcontainer = 0;
+  //       if(int.parse(nums[i]) < int.parse(nums[j] )){
+  //         tempcontainer = int.parse(nums[i]) * (j - i);
+  //       }
+  //       else{
+  //         tempcontainer = int.parse(nums[j]) * (j - i);
+  //       }
+  //       if(container < tempcontainer){
+  //         container = tempcontainer;
+  //       }
+  //       else{
+  //         continue;
+  //       }
+  //     }
+  //   }
+  //   print(container);
 }
